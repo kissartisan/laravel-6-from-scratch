ca2b7eaf98c2e8607239881bf6452f5a565c093a
Reymark Torres <reymark.torres08.dev@gmail.com>
2020-01-04 19:10:30 +0800

    Ep 34: Build a Registration System in Mere Minutes

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,7 @@ Route::post('/articles', 'ArticlesController@store');
 Route::get('/articles/{article}/edit', 'ArticlesController@edit');
 Route::put('/articles/{article}', 'ArticlesController@update');
 Route::delete('/articles/{article}', 'ArticlesController@destroy');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
